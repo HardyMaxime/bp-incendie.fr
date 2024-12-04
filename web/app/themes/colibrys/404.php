7ba859a5e5125ddb7a9d53d98e4a18aa419e8f76
Maxime HARDY <hardymaxime7@gmail.com>
2024-12-04 16:10:24 +0100

    Modification 404

--- web/app/themes/colibrys/404.php
+++ web/app/themes/colibrys/404.php
@@ -4,6 +4,6 @@
     <section class="page-erreur">
         <h1>Erreur 404</h1>
         <p>La page que vous recherchez n'existe pas.</p>
-        <a href="/" title="Retourner à l'accueil" >Retour à l'accueil</a>
+        <a href="<?= esc_url(home_url()); ?>" title="Retourner à l'accueil" >Retour à l'accueil</a>
     </section>
 <?php get_footer(); ?>
