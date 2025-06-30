70997da9400568e2b986b83cd29d1c395e7e1f5c
Maxime HARDY <hardymaxime7@gmail.com>
2025-06-30 15:22:36 +0200

    refactor plugin et vite.config

--- static/vite.config.js
+++ static/vite.config.js
@@ -1,14 +1,16 @@
 import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 import nunjucks from 'vite-plugin-nunjucks'
+import path from 'path'
 
 const THEME = "colibrys";
+const THEME_PATH = '../../web/app/themes/'+THEME+'/assets/ressources/';
 
 export default defineConfig({
     base: "./",
     root: './src',
     build: {
-        outDir: '../../web/app/themes/'+THEME+'/assets/ressources/',
+        outDir: THEME_PATH,
         assetsDir: '',
         assetsInlineLimit: 0,
         rollupOptions: {
@@ -45,4 +47,10 @@ export default defineConfig({
         }),
         nunjucks()
     ],
+    resolve: {
+        alias: {
+            '@images': path.resolve(__dirname, './src/images'),
+            '@': path.resolve(__dirname, './src')
+        }
+    },
 });
--- web/app/themes/colibrys/page.php
+++ web/app/themes/colibrys/page.php
@@ -1,15 +1,6 @@
 <?php get_header(); ?>
-    <div class="section-page container">
-        <section class="section container flex-grow">
-            <?php get_template_part('parts/page/heading', null, array(
-                "title" => DefaultController::getPageHeading(get_the_ID(), 'title'),
-                "description" => DefaultController::getPageHeading(get_the_ID(), 'description'),
-                "class_title" => ['full-title'],
-                "class_subtitle" => ["section-subtitle-full"],
-            )); ?>
-            <div class="section-content mt-5">
-                <?php the_content(); ?>
-            </div>
-        </section>
+    <div class="container">
+        <?= the_title(); ?>
+        <?php the_content(); ?>
     </div>
 <?php get_footer(); ?>
